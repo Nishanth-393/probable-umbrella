b8049f4cbd06ae8cf9686964d44ca98dcf3a6007
Nishanth-393 <103167517+Nishanth-393@users.noreply.github.com>
2022-04-29 16:23:02 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,16 +1,5 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <stdio.h>
-
-int
-main ()
-{
+int main(){
   int n;
   double arr[100];
   printf ("Enter the number of elements (1 to 100): ");
@@ -28,9 +17,8 @@ main ()
 	  arr[0] = arr[i];
 	}
     }
-
   printf ("Largest element = %.2lf", arr[0]);
-
   return 0;
-
 }
+
+// BY Nishanth (RA2111003010829)
